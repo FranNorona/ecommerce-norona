0094b3ef0c93682c99a0e98595f03eab1079ea57
Franco Noroña <franconorona@ootlook.com>
2024-06-13 21:20:47 -0300

    Cambiando colores en navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,21 +4,30 @@ import "./navbar.css";
 const Navbar = () => {
   return (
     <div className="navbarContainer">
-        <div className="logoImg">
-          <img src="https://res.cloudinary.com/dmhprmqnk/image/upload/v1718284613/logowhite_rzpsyd.jpg" alt="logo" />
-        </div>
-        
-        <div className="listContainer">
-            <ul>
-                <li><a href="#">Vasos</a></li>
-                <li><a href="#">Botellas</a></li>
-                <li><a href="#">Deco</a></li>
-            </ul>
-        </div>
+      <div className="logoImg">
+        <img
+          src="https://res.cloudinary.com/dmhprmqnk/image/upload/v1718284613/logoinherit_gos5nh.png"
+          alt="logo"
+        />
+      </div>
 
-        <CartWidget />
+      <div className="listContainer">
+        <ul>
+          <li>
+            <a href="#">Vasos</a>
+          </li>
+          <li>
+            <a href="#">Botellas</a>
+          </li>
+          <li>
+            <a href="#">Deco</a>
+          </li>
+        </ul>
+      </div>
+
+      <CartWidget />
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
