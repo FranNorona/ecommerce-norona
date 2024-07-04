c8fa1d79b01c899446e84de7e139fce56704e164
Franco <franconorona@outlook.com>
2024-07-03 21:47:17 -0300

    Agregando contenido y estilo a itemDetail

--- src/pages/itemDetailContainer/ItemDetail.jsx
+++ src/pages/itemDetailContainer/ItemDetail.jsx
@@ -1,11 +1,32 @@
 import CounterContainer from "../../components/counter/CounterContainer";
+import "./itemdetail.css";
 
 const ItemDetail = ({ item }) => {
   return (
     <>
-      <div style={{ minHeight: "70vh" }}>
-        <h2>{item.title}</h2>
-        <CounterContainer />
+      <div className="itemDetailContainer">
+        <div className="imageContainer">
+          <div className="imageSmallSize">
+            <img src={item.img} alt="imageProduct" />
+          </div>
+          <div className="imageBigSize">
+            <img src={item.img} alt="imageProduct" />
+          </div>
+        </div>
+        <div className="titleContainer">
+          <div className="titleAndDescript">
+            <h1>{item.title}</h1>
+            <div className="stars">
+              <img src="#" alt="#" />
+            </div>
+            <p>{item.description}</p>
+          </div>
+          <div className="buttonAndOfert">
+            <h2>OFERTA</h2>
+            <CounterContainer />
+            <button>Comprar</button>
+          </div>
+        </div>
       </div>
     </>
   );
