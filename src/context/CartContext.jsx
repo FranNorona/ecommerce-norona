f87d229be80b301f8e9b35226292975c47d6c740
Franco <franconorona@outlook.com>
2024-07-28 16:36:08 -0300

    Generando variables de entorno

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -2,45 +2,45 @@ import { createContext, useState } from "react";
 
 export const CartContext = createContext();
 
-const CartContextProvider = ({children}) => {
-    const [cart, setCart] = useState([])
+const CartContextProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
 
-    const addToCart = (product) => {
-        let exists = isInCart(product.id);
-        if (exists) {
-            let newArray = cart.map((elemento) => {
-                if (elemento.id === product.id) {
-                    return {
-                        ...elemento,
-                        quentity: elemento.quantity + product.quantity
-                    };
-                } else {
-                    return elemento;
-                }
-            });
-            setCart(newArray);
+  const addToCart = (product) => {
+    let exists = isInCart(product.id);
+    if (exists) {
+      let newArray = cart.map((elemento) => {
+        if (elemento.id === product.id) {
+          return {
+            ...elemento,
+            quentity: elemento.quantity + product.quantity,
+          };
         } else {
-            setCart([...cart, product]);
+          return elemento;
         }
-    };
+      });
+      setCart(newArray);
+    } else {
+      setCart([...cart, product]);
+    }
+  };
 
-    const clearCart = () => {
-        setCart([]);
-    };
+  const clearCart = () => {
+    setCart([]);
+  };
 
-    const isInCart = (id) => {
-        let exists = cart.some((product) => product.id === id);
-        return exists;
-    };
+  const isInCart = (id) => {
+    let exists = cart.some((product) => product.id === id);
+    return exists;
+  };
 
-    const deleteProduct = (id) => {
-        let newArr = cart.filter((elemento) => elemento.id !== id);
-        setCart(newArr);
-    };
+  const deleteProduct = (id) => {
+    let newArr = cart.filter((elemento) => elemento.id !== id);
+    setCart(newArr);
+  };
 
-    let data = {cart, addToCart, clearCart, deleteProduct};
+  let data = { cart, addToCart, clearCart, deleteProduct };
 
-    return <CartContext.Provider value={data}>{children}</CartContext.Provider>
-}
+  return <CartContext.Provider value={data}>{children}</CartContext.Provider>;
+};
 
 export default CartContextProvider;
